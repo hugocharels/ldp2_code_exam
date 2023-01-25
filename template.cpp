13cd49a5b0b7da89533d4f54beff9c66d4b05320
hugocharels <hugocharels@hotmail.com>
2023-01-25 12:35:47 +0000

    foncteur added

--- template.cpp
+++ template.cpp
@@ -6,14 +6,25 @@ struct Number {
 
 
 template<typename T>
-T add(T a, T b) {
+T add(const T a, const T b) {
 	return a + b;
 }
 
 template<>
-Number add(Number a, Number b) {
+Number add(const Number a, const Number b) {
 	return Number{a.x + b.x}; 
 }
+
+
+template<typename T, typename F>
+T doSomething(T a, T b, F func) {
+	return func(a, b);
+}
+
+template<typename T>
+struct Fonct {
+	T operator()(const T a, const T b) { return a + b; }
+};
 
 
 int main() {
@@ -21,6 +32,8 @@ int main() {
 	std::cout << add(5, 2) << std::endl;
 	std::cout << add(Number{5}, Number{2}).x << std::endl;
 
+	std::cout << doSomething(1, 2, [](const int a, const int b) { return a + b; }) << std::endl;
+	std::cout << doSomething(1, 2, Fonct<int>{}) << std::endl;
 
 	return 0;
 }
