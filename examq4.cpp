8be6450eb8a5b5cc5756c7393b2938a7f975d903
hugocharels <hugocharels@hotmail.com>
2023-01-26 17:04:36 +0000

    r value thing  added

--- examq4.cpp
+++ examq4.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-
 class Evaluable{
 public:
 	virtual double evaluate(double x)=0;
@@ -16,14 +15,18 @@ class Sum : public Evaluable{
 		e1 = other.createCopyE1();
 		e2 = other.createCopyE2();
 	}
+	void move(Sum& other) {
+		delete e1; delete e2;
+		e1 = other.e1; e2 = other.e2;
+		other.e1 = nullptr; other.e2 = nullptr;	
+	}
+
 public:
 	Sum(Evaluable* e1, Evaluable* e2): e1{e1}, e2{e2} {}
 	Sum(const Sum& other) { this->copy(other); }
+	Sum(Sum&& other) { this->move(other); }
 	void operator=(const Sum& other) { delete e1; delete e2; this->copy(other); }
-	void operator=(Sum&& other) {
-		e1 = other.e1; e2 = other.e2;
-		other.e1 = nullptr; other.e2 = nullptr;
-	}
+	void operator=(Sum&& other) { this->move(other); }
 	virtual double evaluate(double x) { return e1->evaluate(x) + e2->evaluate(x); }
 	virtual ~Sum() { delete e1; delete e2; }
 	Sum* createCopy() { return new Sum(e1->createCopy(), e2->createCopy()); }
@@ -39,14 +42,18 @@ class Product : public Evaluable{
 		e1 = other.createCopyE1();
 		e2 = other.createCopyE2();
 	}
+	void move(Product& other) {
+		delete e1; delete e2;
+		e1 = other.e1; e2 = other.e2;
+		other.e1 = nullptr; other.e2 = nullptr;	
+	}
+
 public:
 	Product(Evaluable* e1, Evaluable* e2): e1{e1}, e2{e2} {}
 	Product(const Product& other) { this->copy(other); }
+	Product(Product&& other) { this->move(other); }
 	void operator=(const Product& other) { delete e1; delete e2; this->copy(other); }
-	void operator=(Product&& other) {
-		e1 = other.e1; e2 = other.e2;
-		other.e1 = nullptr; other.e2 = nullptr;
-	}
+	void operator=(Product&& other) { this->move(other); }
 	virtual double evaluate(double x) { return e1->evaluate(x) * e2->evaluate(x); }
 	virtual ~Product() { delete e1; delete e2; }
 	Product* createCopy() { return new Product(e1->createCopy(), e2->createCopy()); }
@@ -74,10 +81,6 @@ public:
 int main(){
 	auto E= new Sum(new Number(3), new Product(new Number(5), new Number(8)));
 	for (int x=0; x<10; x++) std::cout << x << " " << E->evaluate(x) << std::endl;
-	Sum f = *E;
-	auto F = &f;
-	delete E;
-	for (int x=0; x<10; x++) std::cout << x << " " << F->evaluate(x) << std::endl;
 	return 0;
 }
 
